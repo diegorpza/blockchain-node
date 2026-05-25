bdd7d8a530f04f1f77106034ef3b688f79702d32
Diego Rosales Ponza <diego@Diegos-MacBook-Pro-2.local>
2026-05-25 19:53:21 -0400

    Testing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <cstddef>
 #include <iostream>
 #include <vector>
-#include <cryptosetup.h>
 
 using namespace std;
 
@@ -18,8 +17,18 @@ string height;
 class BlockChain {
     public:
         vector<Block> chain;
+
         void addBlock(Block block) {
             chain.push_back(block);
+        }
+
+        bool isValid() {
+            for (int i = 1; i < chain.size(); i++) {
+                if(chain[i].prvHash != chain[i-1].hash) {
+                    return false;
+                }
+            }
+            return true;
         }
 
 };
@@ -35,6 +44,19 @@ int main() {
     genesis.prvHash = "";
     genesis.hash = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f";
 
+    Block block2;
+
+    block2.index = "2";
+    block2.tStamp = "5:50";
+    block2.data = "Diego sends 5";
+    block2.height = "2";
+    block2.prvHash = genesis.hash;
+    block2.hash = "ABC123";
+
+
     BlockChain blockchain;
     blockchain.addBlock(genesis);
+    blockchain.addBlock(block2);
+
+    cout << blockchain.isValid();
 }
